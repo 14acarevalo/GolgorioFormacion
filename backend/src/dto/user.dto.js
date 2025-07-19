d7cf922bbc8826f821983ec4645cdd06c4b41e7f
Albero <alberto.campanero.arevalo@gmail.com>
2025-07-19 09:09:08 +0200

    Version sábado

--- backend/src/dto/user.dto.js
+++ backend/src/dto/user.dto.js
@@ -1,6 +1,6 @@
 function validateUser(user) {
   const errors = [];
-
+//Datos de usuario a utilizar
   if (!user.name) errors.push('El nombre es requerido');
   if (!user.email) errors.push('El email es requerido');
   if (!user.password) errors.push('La contraseña es requerida');
