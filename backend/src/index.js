747edb10de551b020941a54ce68308e43481821a
Albero <alberto.campanero.arevalo@gmail.com>
2025-07-20 17:15:57 +0200

    Version 1 20.07.2025

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 const userRoutes = require('./routes/userRoutes');
 
+// Middleware para leer JSON
 app.use(express.json());
+
+// Usar rutas
 app.use('/api/users', userRoutes);
 
-
-//Creo en una variable el puerto en el que se va a correr el servidor
-const PORT = 3306;
+// Puerto
+const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
